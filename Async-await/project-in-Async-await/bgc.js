7eeb0072ca23ef0486a4db93abca7bb81deeb611
Noor Ahmad <mr.mianwaqasahmad@gmail.com>
2025-05-12 14:19:54 +0500

    add new project usimg  async await

---
+++ Async-await/project-in-Async-await/bgc.js
@@ -0,0 +1,48 @@
+// genarate a random color 
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+let intervalId;
+const startChangingColor = function(){
+   
+    function changeBgColor(){
+        const bgColor = randomColor();
+        const textColor = randomColor();
+
+        document.body.style.backgroundColor = bgColor
+        let colorDisplay = document.querySelector("#colorChange")
+
+        colorDisplay.style.color = textColor;
+        colorDisplay.innerHTML = `Background: ${bgColor} | Text: ${textColor}`;
+    }
+
+
+    if (!intervalId) {  //agr intervalId hai is nhi to
+        intervalId = setInterval(changeBgColor, 2000);
+    }
+
+   
+}
+
+// console.log(randomColor());
+
+document.querySelector("#start").addEventListener("click",()=>{
+    startChangingColor()
+})
+
+
+
+const stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+
+
+document.querySelector("#stop").addEventListener("click",()=>{
+    stopChangingColor()
+})
